8dfa72fde9d4fbe64d82700467d8354089cf4dd5
denden-tri <83654878+denden-tri@users.noreply.github.com>
2022-06-03 16:11:37 +0700

    initial commit

---
+++ app/src/main/java/com/denztri/utspraktik_if6_10119233_dendentriana/MainActivity.java
@@ -0,0 +1,19 @@
+package com.denztri.utspraktik_if6_10119233_dendentriana;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+ *
+ *
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
